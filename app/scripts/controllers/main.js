6e9f40f185b176add6f2716f28fd747f95f3e235
Rupert Redington <rupert@neontribe.co.uk>
2013-03-02 10:37:35 +0000

    First Base
    sudo npm install -g yo bower grunt-cli
    sudo npm install generator-angular generator-testacular
    yo angular
    (add phantomjs: "~1.8.0" to package.json.devDependencies)
    npm install && bower install --dev
    cp testacular.conf.js dev.testacular.conf.js
    (setup dev.testacular.conf watch + phantomjs)

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('puntspotterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Testacular'
+    ];
+  });
